cb3ccb05ae096db015dc41b3c0afd77fbbb1ca02
Chandra Rao <chandrarao.ravi@gmail.com>
2026-06-10 10:43:26 +0530

    Initial Commit, Empty Project structure

---
+++ SiriVoicePayments/SiriVoicePayments/SiriVoicePaymentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriVoicePaymentsApp.swift
+//  SiriVoicePayments
+//
+//  Created by Chandra Rao on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SiriVoicePaymentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SiriVoicePayments/SiriVoicePaymentsTests/SiriVoicePaymentsTests.swift
@@ -0,0 +1,19 @@
+//
+//  SiriVoicePaymentsTests.swift
+//  SiriVoicePaymentsTests
+//
+//  Created by Chandra Rao on 10/06/26.
+//
+
+import Testing
+@testable import SiriVoicePayments
+
+struct SiriVoicePaymentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
